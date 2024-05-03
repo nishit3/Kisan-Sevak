81852ccf6e16d3e468f81ab37b9e1afa71dba34b
Nishit Chaudhary <90385616+nishit3@users.noreply.github.com>
2024-05-03 23:37:26 +0530

    Update cam_module.ino

--- IoT/cam_module.ino
+++ IoT/cam_module.ino
@@ -22,9 +22,9 @@
 #define HREF_GPIO_NUM     23
 #define PCLK_GPIO_NUM     22
 
-const char* ssid = "AQI";
-const char* password = "aqiproject";
-const char* host = "ywqiiurra7.execute-api.ap-south-1.amazonaws.com";
+const char* ssid = "ssid";
+const char* password = "pass";
+const char* host = "ENDPOINT";
 const int httpsPort = 443;
 
 void setup() {
