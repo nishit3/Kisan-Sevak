8b0c648e5c677ba9d536b3eca6026e62b0f87865
Nishit Chaudhary <90385616+nishit3@users.noreply.github.com>
2024-05-03 23:37:00 +0530

    Update home_module.ino

--- IoT/home_module.ino
+++ IoT/home_module.ino
@@ -2,9 +2,9 @@
 #include <WiFiClientSecure.h>
 #include <ArduinoJson.h>
 
-const char* ssid = "AQI";
-const char* password = "aqiproject";
-const char* host = "ywqiiurra7.execute-api.ap-south-1.amazonaws.com";
+const char* ssid = "ssid";
+const char* password = "pass";
+const char* host = "ENDPOINT";
 const int httpsPort = 443;
              
 
