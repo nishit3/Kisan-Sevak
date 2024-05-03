9a9bec52a85d2ec4e5ec664018473015b73f9c33
Nishit Chaudhary <90385616+nishit3@users.noreply.github.com>
2024-05-03 15:01:59 +0530

    Create home-module-notifs-provider.py

---
+++ AWS/Lambda/home-module-notifs-provider.py
@@ -0,0 +1,35 @@
+import boto3
+import json
+
+
+tableName = "home-module-notifs"
+dynamoDB = boto3.resource('dynamodb')
+table = dynamoDB.Table(tableName)
+
+
+def lambda_handler(event, context):
+    response = table.scan()
+    result = response["Items"]
+    
+    while 'LastEvaluatedKey' in response:
+        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+        result.extend(response['Items'])
+
+    body = {
+        "alerts": result
+    }
+    return buildResponse(200, body)
+
+
+def buildResponse(statusCode, body=None):
+    response = {
+        'statusCode': statusCode,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
+    if body is not None:
+        response['body'] = json.dumps(body)
+
+    return response
