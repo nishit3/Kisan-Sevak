390a3450a6a301d93701105b9b6061950242ba40
Jaymin Tanna <92968381+black-shirt@users.noreply.github.com>
2024-05-03 23:19:01 +0530

    add home_module.ino

---
+++ IoT/home_module.ino
@@ -0,0 +1,90 @@
+#include "esp_camera.h"
+#include "base64.h"
+#include <WiFi.h>
+#include <HTTPClient.h>
+#define CAMERA_MODEL_AI_THINKER
+
+
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+const char* ssid = "AQI";
+const char* password = "aqiproject";
+const char* host = "ywqiiurra7.execute-api.ap-south-1.amazonaws.com";
+const int httpsPort = 443;
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+   delay(1000);
+   Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.frame_size = FRAMESIZE_SVGA;
+  config.jpeg_quality = 10;
+  config.fb_count = 2;
+
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    return;
+  }
+}
+
+void loop() {
+  captureAndEncodeToBase64();
+}
+
+void captureAndEncodeToBase64() {
+  
+  camera_fb_t *fb = esp_camera_fb_get();
+  if (fb) {
+    String base64Image = base64::encode(fb->buf, fb->len);
+    esp_camera_fb_return(fb);
+    HTTPClient http;
+    String url = "https://ywqiiurra7.execute-api.ap-south-1.amazonaws.com/prod/upload-storage-img?fmUID=fm1";
+    http.begin(url);
+    http.addHeader("Content-Type", "text/plain");
+    int httpResponseCode = http.POST(base64Image);
+    http.end();
+  } 
+  delay(5000);
+  
+}
