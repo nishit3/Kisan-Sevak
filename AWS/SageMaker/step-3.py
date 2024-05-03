e927e285594bb44ecab4e75fce2cc1731c2a9db1
Nishit Chaudhary <90385616+nishit3@users.noreply.github.com>
2024-05-03 15:50:59 +0530

    Create step-3.py

---
+++ AWS/SageMaker/step-3.py
@@ -0,0 +1,6 @@
+from sagemaker import get_execution_role
+from sagemaker.pytorch import PyTorchModel
+role = get_execution_role()
+pytorch_model = PyTorchModel(model_data='s3://innovation-marathon-models/best_fertilizer_FFN.tar.gz', role=role, 
+                             entry_point='inference.py', py_version='py310', framework_version = '2.1.0')
+predictor = pytorch_model.deploy(instance_type='ml.c4.xlarge', initial_instance_count=1)
