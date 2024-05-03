f2ee91b61413e822f9f3fd767002123d273f7ef8
Nishit Chaudhary <90385616+nishit3@users.noreply.github.com>
2024-05-03 15:01:08 +0530

    Create get-storage-modules-data.py

---
+++ AWS/Lambda/get-storage-modules-data.py
@@ -0,0 +1,36 @@
+import boto3
+import json
+from custom_encoder import CustomEncoder
+
+
+tableName = "Storage-Modules-Data"
+dynamoDB = boto3.resource('dynamodb')
+table = dynamoDB.Table(tableName)
+
+
+def lambda_handler(event, context):
+    response = table.scan()
+    result = response["Items"]
+    
+    while 'LastEvaluatedKey' in response:
+        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+        result.extend(response['Items'])
+
+    body = {
+        "modules": result
+    }
+    return buildResponse(200, body)
+
+
+def buildResponse(statusCode, body=None):
+    response = {
+        'statusCode': statusCode,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
+    if body is not None:
+        response['body'] = json.dumps(body, cls=CustomEncoder)
+
+    return response
